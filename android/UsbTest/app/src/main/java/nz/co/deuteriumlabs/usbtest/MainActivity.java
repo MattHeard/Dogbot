f08c4d4b3b58fea72a7da48bfdc045d62dbabf83
Matt Heard <matt@mattheard.net>
2015-05-31 11:09:59 +1200

    Attempt to accept connection from RPi.

--- android/UsbTest/app/src/main/java/nz/co/deuteriumlabs/usbtest/MainActivity.java
+++ android/UsbTest/app/src/main/java/nz/co/deuteriumlabs/usbtest/MainActivity.java
@@ -6,22 +6,32 @@ import android.content.Intent;
 import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements Runnable {
+
+    private static final String TAG = "MainActivity";
+    UsbAccessory accessory;
+    UsbManager usbManager;
+    ParcelFileDescriptor fileDescriptor;
+    FileInputStream inputStream;
+    FileOutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
         Intent intent = getIntent();
         if (intent != null) {
@@ -32,9 +42,66 @@ public class MainActivity extends Activity {
     private void handleAccessoryIntent(Intent intent) {
         UsbAccessory accessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
         if (accessory != null) {
-            String model_name = accessory.getModel();
-            TextView accessoryModelView = (TextView) findViewById(R.id.accessory_model);
-            accessoryModelView.setText(model_name);
+            identifyAccessory(accessory);
+            enableHelloButton();
+            openAccessory();
+        }
+    }
+
+    private void enableHelloButton() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setEnabled(true);
+    }
+
+    private void identifyAccessory(UsbAccessory accessory) {
+        String model_name = accessory.getModel();
+        TextView accessoryModelView = (TextView) findViewById(R.id.accessory_model);
+        accessoryModelView.setText(model_name);
+    }
+
+    private void openAccessory() {
+        Log.d(TAG, "openAccessory: " + accessory);
+        fileDescriptor = usbManager.openAccessory(accessory);
+        if (fileDescriptor != null) {
+            FileDescriptor fd = fileDescriptor.getFileDescriptor();
+            inputStream = new FileInputStream(fd);
+            outputStream = new FileOutputStream(fd);
+            Thread thread = new Thread(null, this, "AccessoryThread");
+            thread.start();
+        }
+    }
+
+    /**
+     * Starts executing the active part of the class' code. This method is
+     * called when a thread is started that has been created with a class which
+     * implements {@code Runnable}.
+     */
+    @Override
+    public void run() {
+        writeHelloToOutputStream();
+        closeOutputStream();
+    }
+
+    private void writeHelloToOutputStream() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        final String hello = "hello dogbot";
+        try {
+            outputStream.write(hello.getBytes());
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeOutputStream() {
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
