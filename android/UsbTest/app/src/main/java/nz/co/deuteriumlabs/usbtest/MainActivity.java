7213db8fdcf8ea633e5eb97b544a788652975fae
Matt Heard <matt@mattheard.net>
2015-05-31 21:52:11 +1200

    Say "Hello Matt!" when entering accessory mode.

--- android/UsbTest/app/src/main/java/nz/co/deuteriumlabs/usbtest/MainActivity.java
+++ android/UsbTest/app/src/main/java/nz/co/deuteriumlabs/usbtest/MainActivity.java
@@ -5,26 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
-    UsbAccessory accessory;
     UsbManager usbManager;
-    ParcelFileDescriptor fileDescriptor;
-    FileInputStream inputStream;
-    FileOutputStream outputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +34,12 @@ public class MainActivity extends Activity {
         if (accessory != null) {
             identifyAccessory(accessory);
             enableHelloButton();
+            startTestAsync();
         }
+    }
+
+    private void startTestAsync() {
+        new TestAsyncTask().execute();
     }
 
     private void enableHelloButton() {
@@ -56,5 +51,47 @@ public class MainActivity extends Activity {
         String model_name = accessory.getModel();
         TextView accessoryModelView = (TextView) findViewById(R.id.accessory_model);
         accessoryModelView.setText(model_name);
+    }
+
+    private class TestAsyncTask extends AsyncTask<Void, Void, Void> {
+        private String name = "";
+
+        /**
+         * Override this method to perform a computation on a background thread. The
+         * specified parameters are the parameters passed to {@link #execute}
+         * by the caller of this task.
+         * <p/>
+         * This method can call {@link #publishProgress} to publish updates
+         * on the UI thread.
+         *
+         * @param params The parameters of the task.
+         * @return A result, defined by the subclass of this task.
+         * @see #onPreExecute()
+         * @see #onPostExecute
+         * @see #publishProgress
+         */
+        @Override
+        protected Void doInBackground(Void... params) {
+            name = "Matt";
+            return null;
+        }
+
+        /**
+         * <p>Runs on the UI thread after {@link #doInBackground}. The
+         * specified result is the value returned by {@link #doInBackground}.</p>
+         * <p/>
+         * <p>This method won't be invoked if the task was cancelled.</p>
+         *
+         * @param aVoid The result of the operation computed by {@link #doInBackground}.
+         * @see #onPreExecute
+         * @see #doInBackground
+         * @see #onCancelled(Object)
+         */
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            TextView view = (TextView) findViewById(R.id.desc);
+            view.setText("Hello, " + name + "!");
+        }
     }
 }
